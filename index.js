3970ee90584a268046fd0fc67524a94e1f3fd8b3
pablackhawk <l.tirtarahardja@gmail.com>
2018-01-29 20:10:10 -0800

    finished index.js

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
+
+// Routes
+require('./routes/html-routes.js')(app)
+require("'/routes/api-routes.js")(app)
+require('./routes/post-api-routes.js')
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log('App listening on PORT: ' + PORT)
+  })
+})
