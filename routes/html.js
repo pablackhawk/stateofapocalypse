a7cabf8be4c51b98ea5847a4fa78fd7a17e2ae42
pablackhawk <l.tirtarahardja@gmail.com>
2018-02-03 11:23:25 -0800

    added form page route, added links on pages

--- routes/html.js
+++ routes/html.js
@@ -1,14 +1,12 @@
 const path = require('path');
 
 module.exports = function(app) {
-<<<<<<< HEAD
-    console.log('whatever')
-    
-}
-=======
   // index route loads index.html
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
+
+  app.get('/form', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/form.html'));
+  });
 };
->>>>>>> 13a1e68d01a5e7e8c031d3fadbce92991cad9f50
