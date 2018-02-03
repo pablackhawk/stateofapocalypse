20cb145688d0662f86b682007293848345598d6c
Rebecca A. Olson <rebeccaao@dhcp-v074-247.mobile.uci.edu>
2018-02-03 15:18:46 -0800

    Added image_link(s) to schema.js

--- routes/html.js
+++ routes/html.js
@@ -1,10 +1,6 @@
 const path = require('path');
 
 module.exports = function(app) {
-<<<<<<< HEAD:routes/html-routes.js
-  
-=======
->>>>>>> 6031a8b2575b5abb1d5cad8d3178cf5d39400b83:routes/html.js
   // index route loads index.html
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'));
