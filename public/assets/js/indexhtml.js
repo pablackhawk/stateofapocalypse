36e39ec4d5919f8a8ffdae5da7ebbef3c2ce12a4
pablackhawk <l.tirtarahardja@gmail.com>
2018-02-03 15:19:28 -0800

    built card divs

--- public/assets/js/indexhtml.js
+++ public/assets/js/indexhtml.js
@@ -4,14 +4,49 @@ $(document).ready(function() {
     // console.log(event);
     let query = $(this).attr('id');
     console.log(query);
-    // This is to not follow the link
-    return false;
-    let queryURL = 'localhost:5150/products';
+    let queryURL = '/products';
     $.ajax({
       url: queryURL,
       method: 'GET',
     }).done(function(response) {
       console.log(response);
+      if (response < 1) {
+        // Displays message if no products match query
+        $('.product-div').html('<h3>No Products Found!</h3>');
+      } else {
+        // Runs through response array
+        for (let i = 0; i < response.length; i++) {
+          if (query === response[i].category) {
+            let productDiv = $('<div class="col m6 m4">');
+            let productCard = $('<div class="card hoverable">');
+            let productImageDiv = $(
+              '<div class="card-image waves-effect waves-block waves-light">'
+            );
+            // let productImage = $('<img class="activator">');
+            // productImage.attr('src', response[i].image_link);
+            let cardContent = $('<div class="card-content">');
+            let cardTitleSpan = $(
+              '<span class="card-title activator grey-text text-darken-4">'
+            ).text(response[i].product_name);
+            let expandButton = $(
+              '<i class="material-icons right">See More</i>'
+            );
+            if (response[i].video_link !== null) {
+              let videoReview = $('<video class="product-video">');
+              videoReview.attr('src', response[i].video_link);
+              videoReview.append(cardContent);
+            }
+            // productImageDiv.append(productImage);
+            // productCard.append(productImageDiv);
+            cardTitleSpan.append(cardContent);
+            productDiv.append(productCard);
+            productDiv.append(cardContent);
+            $('.product-div').append(productDiv);
+          }
+        }
+      }
     });
+    // This is to not follow the link
+    return false;
   });
 });
