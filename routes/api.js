6031a8b2575b5abb1d5cad8d3178cf5d39400b83
Rebecca A. Olson <rebeccaao@dhcp-v074-247.mobile.uci.edu>
2018-02-03 12:15:08 -0800

    Updated api.js

--- routes/api.js
+++ routes/api.js
@@ -1,38 +1,51 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const db = require('../models');
+const db = require("../models");
 //Connection to the server
 module.exports = function(app) {
-  app.get('/', function(req, res) {
-    res.redirect('/products');
-  });
 
-  //Homepage route
-  app.get('/products', function(req, res) {
-    db.Products.findAll({}).then(function(dbSurvival) {
-      res.json(dbSurvival);
-    });
-  });
+app.get("/", function(req, res) {
+    res.redirect("/products");
+});
 
-  //Specific category route
-  app.get('/products/:category', function(req, res) {
+//Homepage route
+app.get("/products", function(req, res) {
+    db.Products.findAll({})
+        .then(function(dbSurvival) {
+            res.json(dbSurvival);
+        });
+});
+
+
+// Recommendation products route
+app.get("/products/recommend", function(req, res) {
+    db.Products.findOne({
+            where: {
+                category: "knivesandblades", //insert "random" category
+                item_num: Math.floor((Math.random() * 3) + 1) //hard-code based off of "random catgegory"
+                
+            }   
+    })
+    .then(function(dbSurvival) {
+            console.log(dbSurvival);
+            res.json(dbSurvival)
+    });         
+});
+
+
+//Specific category route
+app.get("/products/:category", function(req, res) {
     console.log(req.params.category);
     db.Products.findAll({
-      where: {
-        category: req.params.category,
-      },
-    }).then(function(dbSurvival) {
-      res.json(dbSurvival);
+        where: {
+                category: req.params.category
+        }
+    })
+    .then(function(dbSurvival) {
+            res.json(dbSurvival);
     });
-  });
+});
 
-  //Recommendation products route
-  // app.get("/products/recommend", function(req, res) {
-  //     db.Products.findAll({})
-  //         .then(function(dbSurvival) {
-  //             res.json(dbSurvival);
-  //         });
-  // });
 };
