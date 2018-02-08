918bfacf006340860e5163607ea9db09096f41be
Rebecca A. Olson <rebeccaao@dhcp-v082-186.mobile.uci.edu>
2018-02-07 20:24:15 -0800

    re-added test.js content

--- DB/createProducts.js
+++ DB/createProducts.js
@@ -15,36 +15,6 @@ connection.connect(function(err) {
   displayProducts();
 });
 
-<<<<<<< HEAD
-function displayProducts() {
-    console.log("\n**** STATE OF APOCALYPSE - All Products ****")
-    connection.query("SELECT * FROM Products", function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-            console.log("\n -----------------------------------------------------------------------------")
-            console.log(
-                        "| Item Number: " +
-                        res[i].item_num +
-                        " | Product Name: " +
-                        res[i].product_name +
-                        " | Item Description: " +
-                        res[i].itemDescription +
-                        " | Category: " +
-                        res[i].category +
-                        " | Score: " +
-                        res[i].score +
-                        " | Buy Link: " +
-                        res[i].buy_link +
-                        " | Video Link: " +
-                        res[i].video_link +
-                        " | Image Link: " +
-                        res[i].image_link +
-                        " | Review Link: " +
-                        res[i].review_link);
-            console.log("\n -----------------------------------------------------------------------------");
-        }
-        initialize();
-    });
-=======
 validateInteger = value => {
   let integer = Number.isInteger(parseFloat(value));
   let sign = Math.sign(value);
@@ -54,7 +24,6 @@ validateInteger = value => {
   } else {
     return 'Please enter a whole non-zero number';
   }
->>>>>>> d497dddaaf4dc408c1cdef603b6a7f2f846198b5
 };
 
 displayProducts = () => {
@@ -111,91 +80,6 @@ initialize = () => {
     });
 };
 
-<<<<<<< HEAD
-function deleteProduct() {
-    inquirer.prompt([
-        {
-            name: "addItem",
-            type: "rawlist",
-            message: "Would you like to delete a product from the 'survival_db' database?",
-            choices: ["YES", "NO"] 
-        }
-    ]).then(function(answer) {
-        if (answer.addItem.toUpperCase() === "YES") {
-            deleteItemNumber();
-        } else {
-            console.log("*** Ok, Goodbye!");
-            connection.end(); 
-        }
-    })
-}
-
-function addItem() {
-    inquirer.prompt([{
-            name: "itemNumber",
-            type: "input",
-            message: "Please enter the 'item_number' of the new product."
-        },{
-            name: "productName",
-            type: "input",
-            message: "Please enter the name of the product you would like to add to the database."
-        },{
-            name: "itemDescription",
-            type: "input",
-            message: "Enter the description of the product to store..."
-        },{
-            name: "category",
-            type: "input",
-            message: "What category would you like to place this item in?",
-        },{
-            name: "score",
-            type: "input",
-            message: "What is the editor's score for this product?"
-            //  validate: function(value) {
-            //     if (isNaN(value) === false) {
-            //     return true;
-            //     }
-            //     return false;
-            //     }
-        },{
-            name: "buyLink",
-            type: "input",
-            message: "What is the 'buy_link' you would like to store with this item?"
-        },{
-            name: "videoLink",
-            type: "input",
-            message: "What is the 'video_link' you would like to store with this item?"
-        },{
-            name: "imageLink",
-            type: "input",
-            message: "What is the 'image_link' you would like to store with this item?"
-        },{
-            name: "reviewLink",
-            type: "input",
-            message: "What is the 'review_link' you would like to store with this item?"
-        }
-    ]).then(function(answer) {
-        connection.query("INSERT INTO Products SET ?",
-        [
-            {
-                item_num: answer.itemNumber,
-                product_name: answer.productName,
-                description: answer.itemDescription,
-                category: answer.category,
-                score: answer.score,
-                buy_link: answer.buyLink,
-                video_link: answer.videoLink,
-                image_link: answer.imageLink,
-                review_link: answer.reviewLink
-            }
-        ], function(err) {
-            if (err) throw err;
-
-            console.log("Your product has been successfully added to 'survival_db'!");
-            console.log("===================================================================");
-            resetOption();
-        })
-=======
 addItem = () => {
   inquirer
     .prompt([
@@ -272,7 +156,6 @@ resetOption = () => {
       type: 'list',
       message: 'Have you done all you need?',
       choices: ['YES', 'NO'],
->>>>>>> d497dddaaf4dc408c1cdef603b6a7f2f846198b5
     })
     .then(function(answer) {
       if (answer.done.toUpperCase() === 'YES') {
@@ -309,26 +192,6 @@ deleteProduct = () => {
     });
 };
 
-<<<<<<< HEAD
-
-function deleteItemNumber() {
-    inquirer.prompt([
-        {
-            name: "itemNumberDelete",
-            type: "input",
-            message: "Please enter the 'item_number' of the product you would like to remove from the 'survival_db'"
-        }
-    ]).then(function(answer) {
-        console.log("Deleting product...\n");
-        connection.query("DELETE FROM Products WHERE ?", { item_num: answer.itemNumberDelete }, 
-            function(err) {
-                if (err) throw err;
-                console.log("Product deleted!");
-                initialize();
-        })
-    })
-  }
-=======
 function deleteItemNumber() {
   inquirer
     .prompt([
@@ -352,4 +215,3 @@ function deleteItemNumber() {
       );
     });
 }
->>>>>>> d497dddaaf4dc408c1cdef603b6a7f2f846198b5
---
+++ test/test.js
@@ -0,0 +1,22 @@
+var Nightmare = require("nightmare");
+var expect = require("chai").expect;
+
+describe("State of Apocalypse", function () {
+    // The default tests in mocha is 2 seconds.
+    // Extending it to 30 seconds to have time to load the pages
+
+    this.timeout(30000);
+    it("should send user to the home page, then the form page", function (done) {
+        // ID for the login button.
+        new Nightmare({ show: true })
+            .goto("http://stateofapocalypse.herokuapp.com/")
+            // Click the form "Get Started" link
+            .click("#download-button")
+            //Wait 10 seconds
+            .wait(10000)
+            .then(function () {
+                done();
+            });
+
+    })
+});
