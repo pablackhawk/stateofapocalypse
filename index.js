d5607ad6ffc3dc779cb7ca83656a72fb3367a06a
pablackhawk <l.tirtarahardja@gmail.com>
2018-02-17 04:27:35 -0800

    CSS tweak

--- index.js
+++ index.js
@@ -5,26 +5,25 @@ const methodOverride = require('method-override');
 
 // Express App
 let app = express();
-const PORT = process.env.PORT || 5150
+const PORT = process.env.PORT || 5150;
 
 // Models
-let db = require('./models') 
+let db = require('./models');
 
 // Routes
-require('./routes/html.js')(app)
-require('./routes/api.js')(app)
-require('./routes/scraper.js')(app)
+require('./routes/html.js')(app);
+require('./routes/api.js')(app);
+require('./routes/scraper.js')(app);
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(methodOverride('_method'))
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
 
 db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
-    console.log('App listening on PORT: ' + PORT)
-  })
-})
+    console.log('App listening on PORT: ' + PORT);
+  });
+});
 
 exports = module.exports = app;
